92bc8587d518f89c647f4cce6bf76a7ea612a31e
Beatrix Haddock <bhaddock@uefi24-gold.pc.scharp.org>
2026-08-23 12:57:44 -0700

    models

--- single_mabs/4PL_plate_fit/v4/run_models_parallel_rsrv.R
+++ single_mabs/4PL_plate_fit/v4/run_models_parallel_rsrv.R
@@ -10,7 +10,7 @@ library(ps)
 library(parallel)
 library(dplyr)
 
-models_dir <- "/home/bhaddock/repos/titration_pnlme/single_mabs/4PL_plate_fit/v3/model_files"
+models_dir <- "/home/bhaddock/repos/titration_pnlme/single_mabs/4PL_plate_fit/v4/model_files"
 model_files <- list.files(models_dir, pattern = "^m[0-9]+\\.mlxtran$")
 model_names <- sub("^(m[0-9]+)\\.mlxtran$", "\\1", model_files)
 model_names <- model_names[order(as.integer(sub("^m", "", model_names)))]
@@ -108,7 +108,7 @@ run_one_model <- function(model_name, models_dir) {
   invisible(model_name)
 }
 
-n_workers <- 2 
+n_workers <- 1 
 cl <- makeCluster(n_workers)
 
 results <- tryCatch(
