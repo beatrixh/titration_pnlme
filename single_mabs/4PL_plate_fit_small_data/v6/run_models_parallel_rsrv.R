6f526166e91d7682e65d89cf5d6b90e296f5f586
Beatrix Haddock <bhaddock@uefi24-gold.pc.scharp.org>
2026-08-30 23:13:47 -0700

    models 1-15 done

--- single_mabs/4PL_plate_fit_small_data/v6/run_models_parallel_rsrv.R
+++ single_mabs/4PL_plate_fit_small_data/v6/run_models_parallel_rsrv.R
@@ -15,7 +15,7 @@ model_files <- list.files(models_dir, pattern = "^m[0-9]+\\.mlxtran$")
 model_names <- sub("^(m[0-9]+)\\.mlxtran$", "\\1", model_files)
 model_names <- model_names[order(as.integer(sub("^m", "", model_names)))]
 
-model_names <- paste0("m", 1:15)
+model_names <- paste0("m", 16:512)
 
 run_one_model <- function(model_name, models_dir) {
   library(lixoftConnectors)
@@ -108,7 +108,7 @@ run_one_model <- function(model_name, models_dir) {
   invisible(model_name)
 }
 
-n_workers <- 2 
+n_workers <- 4 
 cl <- makeCluster(n_workers)
 
 results <- tryCatch(
